0384b9fa99e3325c83c57f27b569b0f5263f9318
Xenox003 <xenox003yt@gmail.com>
2020-10-24 21:43:39 +0200

    updated MySQLManager

--- src/main/java/de/xenox/xenoxbot/manager/MySQLManager.java
+++ src/main/java/de/xenox/xenoxbot/manager/MySQLManager.java
@@ -1,13 +1,30 @@
 package de.xenox.xenoxbot.manager;
 
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class MySQLManager {
-    public static void connect(String database, String username, String password) {
+    public static Connection con = null;
+    public static Statement statement = null;
 
+    public static void connect(String database, String username, String password) {
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection("jdbc:mysql//" + database, username, password);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
     public static void disconnect() {
-
+        if (con != null) {
+            try {
+                con.close();
+                con = null;
+                statement = null;
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
     public static void onCommand(String sql) {
 
