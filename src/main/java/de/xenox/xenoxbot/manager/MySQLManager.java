7da0092e5dc868df8902d5c4d483547e40336f10
Xenox003 <xenox003yt@gmail.com>
2020-10-24 21:29:48 +0200

    added CommandManager and MySQLManager

---
+++ src/main/java/de/xenox/xenoxbot/manager/MySQLManager.java
@@ -0,0 +1,19 @@
+package de.xenox.xenoxbot.manager;
+
+import java.sql.ResultSet;
+
+public class MySQLManager {
+    public static void connect(String database, String username, String password) {
+
+    }
+    public static void disconnect() {
+
+    }
+    public static void onCommand(String sql) {
+
+    }
+    public static ResultSet onQuery(String sql) {
+
+        return null;
+    }
+}
